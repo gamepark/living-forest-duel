a3b7dec2842302fbab05000f4773a7b7fb1aef0a
Romain Fromi <romain.fromi@gmail.com>
2024-12-11 14:50:33 +0100

    Add z coordinates on PlayerForestLocator

--- app/src/locators/PlayerForestLocator.ts
+++ app/src/locators/PlayerForestLocator.ts
@@ -13,7 +13,8 @@ class PlayerForestLocator extends DeckLocator {
     
     return {
       x: x + (location.x! - deltaX) * (treeCardDescription.width + 0.2),
-      y: y + (location.y! - deltaY) * (treeCardDescription.height + 0.2)
+      y: y + (location.y! - deltaY) * (treeCardDescription.height + 0.2),
+      z: (location.z ?? 0) * 0.05
     }
   }
 }
