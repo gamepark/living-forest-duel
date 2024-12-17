ece172ae25fa9de5337c156f9e04cfe9d65dad0f
Romain Fromi <romain.fromi@gmail.com>
2024-12-17 14:07:57 +0100

    Bugfix: if opponent has 2 action tokens and loses 1, and I have 1 that I do not lose, I should be offered to use the Sanki card

--- rules/src/rules/PlayerActionRule.ts
+++ rules/src/rules/PlayerActionRule.ts
@@ -12,7 +12,7 @@ import { RuleId } from './RuleId'
 export class PlayerActionRule extends PlayerUseActionTokenRule {
   getPlayerMoves() {
     const moves: MaterialMove[] = []
-    
+
     // Add one card to the shared help line
     const sharedDeck = this.material(MaterialType.AnimalCard).location(LocationType.SharedDeck).deck()
     moves.push(sharedDeck.dealOne({ type: LocationType.SharedHelpLine }))
@@ -79,14 +79,8 @@ export class PlayerActionRule extends PlayerUseActionTokenRule {
         }
       }
     }
-    // If the player has enough action tokens and a Sanki card, offer using it if it's not already been offered because of a fire Varan
-    // The action tokens have not moved here yet, that's why I need to check the last move
-    // We don't offer it in case the other player doesn't have any action token, as it wouldn't have sense using the Sanki card
-    const actionTokens = this.material(MaterialType.ActionToken).location(LocationType.PlayerActionSupply).player(this.player)
-    if ((actionTokens.getQuantity() == 2 || (actionTokens.getQuantity() == 1 && moves.length > 0 && !isMoveItemType(MaterialType.ActionToken)(moves[moves.length - 1])))
-      && this.playerHasSankiCard(this.player)
-      && new PlayerUseActionTokenRule(this.game).getPlayerMoves().length > 0
-      && this.material(MaterialType.ActionToken).location(LocationType.PlayerActionSupply).player(this.nextPlayer).getItems().length > 0) {
+    if (this.playerHasSankiCard(this.player)
+      && this.material(MaterialType.ActionToken).location(LocationType.PlayerActionSupply).player(this.nextPlayer).length > 0) {
       moves.push(this.startRule(RuleId.UseSankiCard))
     } else {
       moves.push(this.startRule(RuleId.EndTurn))
