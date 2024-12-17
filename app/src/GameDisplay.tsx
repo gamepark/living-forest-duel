8492e249903b281b2bc520fa3018b3957fffa607
Romain Fromi <romain.fromi@gmail.com>
2024-12-17 17:43:50 +0100

    Adapt table size so that forest can be more than 3 cards in width

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,16 +1,23 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { TreesHelper } from '@gamepark/living-forest-duel/rules/helpers/TreesHelper'
+import { Season } from '@gamepark/living-forest-duel/Season'
 import { GameTable, GameTableNavigation } from '@gamepark/react-game'
-import { FC } from 'react'
+import { MaterialGame } from '@gamepark/rules-api'
+import { playerForestLocator } from './locators/PlayerForestLocator'
 import { PlayerPanels } from './panels/PlayerPanels'
 
 type GameDisplayProps = {
-  players: number
+  game: MaterialGame
 }
 
-export const GameDisplay: FC<GameDisplayProps> = () => {
+export const GameDisplay = ({game}: GameDisplayProps) => {
+  const summerForestBoundaries = new TreesHelper(game, Season.Summer).boundaries
+  const winterForestBoundaries = new TreesHelper(game, Season.Winter).boundaries
+  const xMin = -70 - Math.max(summerForestBoundaries.xMax - summerForestBoundaries.xMin - 2, 0) * playerForestLocator.gridGap
+  const xMax = 70 + Math.max(winterForestBoundaries.xMax - winterForestBoundaries.xMin - 2, 0) * playerForestLocator.gridGap
   return <>
-    <GameTable xMin={-63} xMax={63} yMin={-30} yMax={33}
+    <GameTable xMin={xMin} xMax={xMax} yMin={-30} yMax={33}
       margin={{ top: 7, left: 0, right: 0, bottom: 0 }}
       css={process.env.NODE_ENV === 'development' && css`border: 1px solid white;`}>
       <GameTableNavigation />
--- app/src/locators/PlayerForestLocator.ts
+++ app/src/locators/PlayerForestLocator.ts
@@ -5,15 +5,18 @@ import { Location } from "@gamepark/rules-api"
 import { treeCardDescription } from "../material/TreeCardDescription"
 
 class PlayerForestLocator extends DeckLocator {
+  gridGap = treeCardDescription.width + 0.2
+
   getCoordinates(location: Location, context: MaterialContext) {
     const { xMax, xMin, yMax, yMin } = new TreesHelper(context.rules.game, location.player!).boundaries
-    const { x, y } = { x: location.player === Season.Summer ? -48 : 48, y: -10 }
+    const baseX = 52 + Math.max(xMax - xMin - 2, 0) * this.gridGap / 2
+    const x = location.player === Season.Summer ? -baseX : baseX
     const deltaX = (xMin + xMax) / 2
     const deltaY = (yMin + yMax) / 2
     
     return {
-      x: x + (location.x! - deltaX) * (treeCardDescription.width + 0.2),
-      y: y + (location.y! - deltaY) * (treeCardDescription.height + 0.2),
+      x: x + (location.x! - deltaX) * this.gridGap,
+      y: -10 + (location.y! - deltaY) * this.gridGap,
       z: (location.z ?? 0) * 0.05
     }
   }
