13423e949b4c153ce4e487008eada7bdeca5c6cc
Romain Fromi <romain.fromi@gmail.com>
2024-12-18 13:57:58 +0100

    Simplify algorithm to prevent player from taking the same element twice without an arrow position in between

--- rules/src/rules/PlayerUseActionTokenRule.ts
+++ rules/src/rules/PlayerUseActionTokenRule.ts
@@ -2,7 +2,7 @@ import { isMoveItemType, ItemMove, MaterialMove, PlayerTurnRule } from '@gamepar
 import { Animal, animalProperties, CardElements } from '../material/Animal'
 import { LocationType } from '../material/LocationType'
 import { MaterialType } from '../material/MaterialType'
-import { Element, elements, Season } from '../Season'
+import { Element, elements } from '../Season'
 import { AnimalsHelper } from './helpers/AnimalsHelper'
 import { ElementsHelper } from './helpers/ElementsHelper'
 import { FireHelper } from './helpers/FireHelper'
@@ -27,7 +27,7 @@ export class PlayerUseActionTokenRule extends PlayerTurnRule {
 
   getAvailableActions() {
     const moves: MaterialMove[] = []
-    const sharedCards = this.material(MaterialType.AnimalCard).location(LocationType.SharedHelpLine).sort(item => -item.location.x!).getItems<Animal>()
+    const sharedCards = this.material(MaterialType.AnimalCard).location(LocationType.SharedHelpLine).sort(item => -item.location.x!)
     // Using the CardElements type to store the id of the animal card id that is available in this action
     const maxElements: CardElements = {
       [Element.Sun]: -1,
@@ -36,7 +36,7 @@ export class PlayerUseActionTokenRule extends PlayerTurnRule {
       [Element.Wind]: -1
     }
 
-    for (const card of sharedCards) {
+    for (const card of sharedCards.getItems<Animal>()) {
       const cardProperties = animalProperties[card.id]
       for (const element of elements) {
         if (maxElements[element]! < 0 && cardProperties.elements[element] !== undefined) {
@@ -51,23 +51,12 @@ export class PlayerUseActionTokenRule extends PlayerTurnRule {
     }
 
     // Validate the positions. At least 1 element between the action token and the previous one.
-    for (const element of elements) {
-      let cardsWithElement = 0
-      for (let x = this.material(MaterialType.AnimalCard).id(maxElements[element]).getItem()?.location.x; x! >= 0; x!--) {
-        const card = this.material(MaterialType.AnimalCard).location(l => l.type === LocationType.SharedHelpLine && l.x === x).getItem<Animal>()!
-        const cardProperties = animalProperties[card.id]
-        if (cardProperties.elements[element] !== undefined) {
-          const actionToken = this.material(MaterialType.ActionToken).location(l => l.type === LocationType.ActionToken && l.x === card.location.x && l.y === element).getItem<Season>()
-          if (actionToken) {
-            if (actionToken.id === this.player || cardsWithElement === 0) {
-              maxElements[element] = -1
-            }
-            break
-          } else {
-            cardsWithElement++
-            if (cardsWithElement == 2) break
-          }
-        }
+    const actionTokenOnCard = this.material(MaterialType.ActionToken).id(this.player).location(LocationType.ActionToken).getItem()
+    if (actionTokenOnCard) {
+      const element = actionTokenOnCard.location.y as Element
+      const cardsAfterToken = sharedCards.location(l => l.x! > actionTokenOnCard.location.x!)
+      if (cardsAfterToken.id<Animal>(animal => animalProperties[animal].elements[element] !== undefined).length < 2) {
+        maxElements[element] = -1
       }
     }
 
