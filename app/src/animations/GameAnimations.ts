6b98b8b53406dbe1b69ae3c9dfcc70aa24bbbc85
Romain Fromi <romain.fromi@gmail.com>
2025-01-10 10:17:17 +0100

    Faster animations for retrieving action tokens

--- app/src/animations/GameAnimations.ts
+++ app/src/animations/GameAnimations.ts
@@ -1,3 +1,10 @@
+import { LocationType } from '@gamepark/living-forest-duel/material/LocationType'
+import { MaterialType } from '@gamepark/living-forest-duel/material/MaterialType'
 import { MaterialGameAnimations } from '@gamepark/react-game'
+import { isMoveItemType } from '@gamepark/rules-api'
 
 export const gameAnimations = new MaterialGameAnimations()
+
+gameAnimations.when()
+  .move(move => isMoveItemType(MaterialType.ActionToken)(move) && move.location.type === LocationType.PlayerActionSupply)
+  .duration(0.5)
