f3dad92cae549c5b5c9c0547e0f47a79f60aac25
Romain Fromi <romain.fromi@gmail.com>
2025-02-06 09:15:53 +0100

    Auto pass on use sanki rule

--- app/src/headers/UseSankiCardHeader.tsx
+++ app/src/headers/UseSankiCardHeader.tsx
@@ -1,8 +1,8 @@
 /** @jsxImportSource @emotion/react */
-
 import { LivingForestDuelRules } from "@gamepark/living-forest-duel/LivingForestDuelRules"
 import { MaterialType } from "@gamepark/living-forest-duel/material/MaterialType"
 import { CustomMoveType } from "@gamepark/living-forest-duel/rules/CustomMoveType"
+import { RuleId } from '@gamepark/living-forest-duel/rules/RuleId'
 import { useRules, usePlayerId, usePlayerName, useLegalMove, PlayMoveButton } from "@gamepark/react-game"
 import { isCustomMoveType, isMoveItemType } from "@gamepark/rules-api"
 import { Trans } from "react-i18next"
@@ -20,7 +20,7 @@ export const UseSankiCardHeader = () => {
     return (
       <Trans defaults="header.use-sanki-card.you" components={{
         useSanki: <PlayMoveButton move={useSanki} />,
-        pass: <PlayMoveButton move={pass} />
+        pass: <PlayMoveButton move={pass} auto={rules.game.rule?.id === RuleId.UseSankiPlayAction ? 10 : undefined}/>
       }} />
     )
   } else {
